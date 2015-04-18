2ecea0fe1940ee5394f5ac970b06a62e2fbc08fb
Simon Lammer <lammer.simon@gmail.com>
2015-04-18 12:23:22 +0200

    Added license info to WatchService_Test.java

--- src/com/github/simonlammer/syncopto/tests/WatchService_Test.java
+++ src/com/github/simonlammer/syncopto/tests/WatchService_Test.java
@@ -1,3 +1,20 @@
+/*
+This file is part of Syncopto. © 2015 Simon Lammer (lammer.simon@gmail.com)
+
+Syncopto is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Syncopto is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Syncopto.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.github.simonlammer.syncopto.tests;
 
 import org.junit.*;
@@ -8,9 +25,6 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.util.List;
 
-/**
- * Created by lamme_000 on 13.04.2015.
- */
 public class WatchService_Test {
     private static File dir;
     private static Path dirPath;
