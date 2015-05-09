ac3da95f0f0e4b236425f4f462ede9fa42ca3481
Simon Lammer <lammer.simon@gmail.com>
2015-05-09 09:48:45 +0200

    Added Link with method stubs

---
+++ src/com/github/simonlammer/syncopto/logic/Link.java
@@ -0,0 +1,83 @@
+/*
+This file is part of Syncopto. © 2015 Simon Lammer (lammer.simon@gmail.com)
+
+Syncopto is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Syncopto is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Syncopto.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.simonlammer.syncopto.logic;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Link {
+    private File from; //directory
+    private File to; //directory
+    private List<Filter> filters;
+    private LinkMode mode;
+    private String name;
+
+    // TODO implement constructors
+
+    public void addFilter(Filter filter) {
+        filters.add(filter);
+    }
+
+    public List<Filter> getFilters() {
+        return new ArrayList<>(filters); // return a clone of the list
+    }
+
+    public LinkMode getMode() {
+        return mode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setMode(LinkMode mode) {
+        this.mode = mode;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFilters(List<Filter> filters) {
+        this.filters = filters;
+    }
+
+    public void upateFileLinks(){
+        throw new NotImplementedException(); //TODO
+    }
+
+    public void removeAllFilters() {
+        filters.clear();
+    }
+
+    public boolean removeFilter(Filter filter) {
+        return filters.remove(filter);
+    }
+
+    public Filter removeFilter(int index) {
+        return filters.remove(index);
+    }
+
+    public boolean removeFilterIf(Predicate<? super Filter> predicate) {
+        return filters.removeIf(predicate);
+    }
+}
