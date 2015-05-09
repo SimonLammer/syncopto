e9d320c3f238a133695c3414f252023fb9b8e952
Simon Lammer <lammer.simon@gmail.com>
2015-05-09 11:14:17 +0200

    Commented all methods in Link

--- src/com/github/simonlammer/syncopto/logic/Link.java
+++ src/com/github/simonlammer/syncopto/logic/Link.java
@@ -32,65 +32,166 @@ public class Link {
     private LinkMode mode;
     private String name;
 
+    /**
+     * Simple private constructor to reduce code duplicates in other constructors
+     * @param name Link's name
+     * @param mode Link's mode
+     * @param filters Collection of filters, which the Link uses to connect two directories
+     */
     private Link(String name, LinkMode mode, Collection<Filter> filters) {
         setName(name);
         setMode(mode);
         setFilters(filters);
     }
+
+    /**
+     * This constructor does not set any filters.
+     * @param name Link's name
+     * @param mode Link's mode
+     * @param originDirectoryPath Path to the origin directory as a String
+     * @param destinationDirectoryPath Path to the destination directory as a String
+     */
     public Link(String name, LinkMode mode, String originDirectoryPath, String destinationDirectoryPath) { this(name, mode, originDirectoryPath, destinationDirectoryPath, null); }
+
+    /**
+     *
+     * @param name Link's name
+     * @param mode Link's mode
+     * @param originDirectoryPath Path to the origin directory as a String
+     * @param destinationDirectoryPath Path to the destination directory as a String
+     * @param filters Collection of filters, which the Link uses to connect two directories
+     */
     public Link(String name, LinkMode mode, String originDirectoryPath, String destinationDirectoryPath, Collection<Filter> filters) {
         this(name, mode, filters);
         setOriginDirectory(originDirectoryPath);
         setDestinationDirectory(destinationDirectoryPath);
     }
+
+    /**
+     * This constructor does not set any filters.
+     * @param name Link's name
+     * @param mode Link's mode
+     * @param originDirectory Origin directory
+     * @param destinationDirectory Destination directory
+     */
     public Link(String name, LinkMode mode, File originDirectory, File destinationDirectory) { this(name, mode, originDirectory, destinationDirectory, null); }
+
+    /**
+     *
+     * @param name Link's name
+     * @param mode Link's mode
+     * @param originDirectory Origin directory
+     * @param destinationDirectory Destination directory
+     * @param filters Collection of filters, which the Link uses to connect two directories
+     */
     public Link(String name, LinkMode mode, File originDirectory, File destinationDirectory, Collection<Filter> filters) {
         this(name, mode, filters);
         setOriginDirectory(originDirectory);
         setDestinationDirectory(destinationDirectory);
     }
 
+    /**
+     * Adds all Filters
+     * @param filters Filters to be added
+     */
+    public void addAllFilters(Collection<Filter> filters) {
+        filters.addAll(filters);
+    }
+
+    /**
+     * Adds the Filter to the Link
+     * @param filter Filter to be added
+     */
     public void addFilter(Filter filter) {
         filters.add(filter);
     }
 
+    /**
+     * Retrieves the destination directory
+     * @return Destination directory
+     */
     public File getDestinationDirectory() {
         return destination;
     }
 
+    /**
+     * Retrieves all Filters of the Link
+     * @return All Link's Filters
+     */
     public List<Filter> getFilters() {
         return new ArrayList<>(filters); // return a clone of the list
     }
 
+    /**
+     * Retrieves the mode
+     * @return Mode
+     */
     public LinkMode getMode() {
         return mode;
     }
 
+    /**
+     * Retrieves the name
+     * @return Name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Retrieves the origin directory
+     * @return Origin directory
+     */
     public File getOriginDirectory() {
         return origin;
     }
 
+    /**
+     * Removes all Filters from Link
+     */
     public void removeAllFilters() {
         filters.clear();
     }
 
+    /**
+     * Removes a Filter from the Link
+     * @param filter Filter to be removed
+     * @return Whether or not the removal was successful
+     */
     public boolean removeFilter(Filter filter) {
         return filters.remove(filter);
     }
 
+    /**
+     * Removes a Filter from the Link
+     * @param index Index of the Filter to be removed
+     * @return Whether or not the removal was successful
+     */
     public Filter removeFilter(int index) {
         return filters.remove(index);
     }
 
+    /**
+     * Removes all Filters of the Link that match the criteria
+     * @param predicate Criteria that determines whether a Filter gets removed
+     * @return {@code true} if any elements were removed
+     */
     public boolean removeFilterIf(Predicate<? super Filter> predicate) {
         return filters.removeIf(predicate);
     }
 
+    /**
+     * Sets the destination directory
+     * @param dirPath Path of to the new destination directory as a String
+     * @return Whether dirPath leads to a directory in which the the Link is granted writing permissions
+     */
     public boolean setDestinationDirectory(String dirPath) { return setDestinationDirectory(new File(dirPath)); }
+
+    /**
+     * Sets the destination directory
+     * @param dir New destination directory
+     * @return Whether dirPath leads to a directory in which the the Link is granted writing permissions
+     */
     public boolean setDestinationDirectory(File dir) {
         if (dir.exists() && dir.isDirectory() && dir.canWrite()) {
             this.destination = dir;
@@ -100,10 +201,19 @@ public class Link {
         }
     }
 
+    /**
+     * Sets the mode
+     * @param mode New Mode
+     */
     public void setMode(LinkMode mode) {
         this.mode = mode;
     }
 
+    /**
+     * Sets the name
+     * @param name New name
+     * @return Whether the name is valid (Neither null nor empty)
+     */
     public boolean setName(String name) {
         if (name != null && !name.isEmpty()) {
             this.name = name;
@@ -113,7 +223,18 @@ public class Link {
         }
     }
 
+    /**
+     * Sets the origin directory
+     * @param dirPath Path of to the new origin directory as a String
+     * @return Whether dirPath leads to a directory in which the the Link is granted reading permissions
+     */
     public boolean setOriginDirectory(String dirPath) { return setOriginDirectory(new File(dirPath)); }
+
+    /**
+     * Sets the origin directory
+     * @param dir New origin directory
+     * @return Whether dirPath leads to a directory in which the the Link is granted reading permissions
+     */
     public boolean setOriginDirectory(File dir) {
         if (dir.exists() && dir.isDirectory() && dir.canRead()) {
             this.origin = dir;
@@ -123,10 +244,18 @@ public class Link {
         }
     }
 
+    /**
+     * Removes all current filters and adds the passed filters afterwards
+     * @param filters New Filters
+     */
     public void setFilters(Collection<Filter> filters) {
-        this.filters = new ArrayList<>(filters);
+        removeAllFilters();
+        addAllFilters(filters);
     }
 
+    /**
+     * Updates the connection between origin and destination directory
+     */
     public void upateFileLinks(){
         throw new NotImplementedException(); //TODO
     }
